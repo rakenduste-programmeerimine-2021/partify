4a6afffe43d0cc71b773dd72dd9103376ea39f34
karljanar <karljk@tlu.ee>
2021-12-04 14:44:21 +0200

    closes #12; Votes controller tests

---
+++ server/src/test/like.user.test.js
@@ -0,0 +1,31 @@
+const votesController = require('../controllers/vote.controller')
+var expect = require('chai').expect
+
+
+
+describe('User likes another user', () => {
+    var req = {
+        userId: "53aae1d126d57c198d861cfd",
+        params: {
+            id: "53aae1d126d57c198d861cfd",
+        }
+    };
+
+    var res = {
+        sendCalledWith: '',
+        send: function (arg) {
+            this.sendCalledWith = arg;
+        },
+        json: function (err) {
+            console.log("\n : " + err);
+        },
+        status: function (s) {
+            this.statusCode = s;
+            return this;
+        }
+    };
+    it("Should work, because it updates or creates new entry, doesn't check for post", async function() {
+        await votesController.likeUser(req, res)
+        expect(res.statusCode).to.equal(200)
+    });
+});
